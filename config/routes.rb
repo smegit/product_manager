d0c9187dcacbf3d467b1f8ae7938a469333f7f7d
Jesse Zha <jinji.zha@gmail.com>
2018-08-30 10:19:14 +1000

    replace \r\n to \n in permitted_params.rb and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,88 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  resources :aesthetics
-  resources :functions
-  resources :system_controls
-  resources :approvals
-  mount Ckeditor::Engine => '/ckeditor'
-  devise_for :users, controllers: { sessions: 'sessions',
-                                    registrations: 'registrations',
-                                    passwords: 'passwords' }
-
-  unauthenticated :user do
-    root to: 'pages#index', id: 'index'
-  end
-
-  devise_scope :user do
-    get 'login',  to: 'sessions#new'
-    get 'logout', to: 'sessions#destroy'
-    get 'register', to: 'devise/registrations#new'
-
-    authenticate :user, ->(u) { u.admin? } do
-      root to: 'products#index', as: :authenticated_admin
-    end
-  end
-
-  resources :products do
-    member do
-      get :history
-    end
-  end
-  resources :features
-
-  resource :searchable, only: [] do
-    collection do
-      get :features
-    end
-  end
-
-  root to: 'pages#index'
-
-  mount API::Base => '/'
-  mount GrapeSwaggerRails::Engine => '/apidoc'
-end
+# frozen_string_literal: true
+
+
+
+Rails.application.routes.draw do
+
+  resources :aesthetics
+
+  resources :functions
+
+  resources :system_controls
+
+  resources :approvals
+
+  mount Ckeditor::Engine => '/ckeditor'
+
+  devise_for :users, controllers: { sessions: 'sessions',
+
+                                    registrations: 'registrations',
+
+                                    passwords: 'passwords' }
+
+
+
+  unauthenticated :user do
+
+    root to: 'pages#index', id: 'index'
+
+  end
+
+
+
+  devise_scope :user do
+
+    get 'login',  to: 'sessions#new'
+
+    get 'logout', to: 'sessions#destroy'
+
+    get 'register', to: 'devise/registrations#new'
+
+
+
+    authenticate :user, ->(u) { u.admin? } do
+
+      root to: 'products#index', as: :authenticated_admin
+
+    end
+
+  end
+
+
+
+  resources :products do
+
+    member do
+
+      get :history
+
+    end
+
+  end
+
+  resources :features
+
+
+
+  resource :searchable, only: [] do
+
+    collection do
+
+      get :features
+
+    end
+
+  end
+
+
+
+  root to: 'pages#index'
+
+
+
+  mount API::Base => '/'
+
+  mount GrapeSwaggerRails::Engine => '/apidoc'
+
+end
+
