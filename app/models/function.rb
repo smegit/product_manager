23afa03dc12bb222dbe67ed6074c6d0ba0330ed5
Jesse Zha <jinji.zha@gmail.com>
2018-09-07 11:08:54 +1000

    remove unnecessary space

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,9 +1,12 @@
-class Function < ApplicationRecord
-  include PgSearch
-
-  pg_search_scope :search_functions,
-                  against: %i[product_type description],
-                  using: { tsearch: { prefix: true, any_word: false } }
-
-  mount_uploader :url, FunctionIconUploader
-end
+class Function < ApplicationRecord
+  include PgSearch
+  pg_search_scope :search_functions,
+                  against: %i[product_type description],
+                  using: { tsearch: { prefix: true, any_word: false } }
+  mount_uploader :url, FunctionIconUploader
+end
+
+
+
+
+
