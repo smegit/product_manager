e6a0c6d088d6f8ed78e7e6a1b16082a9b6f4c8ae
Jesse Zha <jinji.zha@gmail.com>
2018-09-10 09:29:32 +1000

    change doc uploader to save original file instead of jpg

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -1,9 +1,16 @@
-class DocumentUploader < BaseUploader
+class DocumentUploader < CarrierWave::Uploader::Base
 
   # process :store_dimensions
 
+  storage :file
+
   def extension_white_list
     %w(jpg jpeg gif png pdf)
+  end
+
+  def store_dir
+    puts "#{model.class.to_s.underscore} #{mounted_as}   #{model.id}"
+    "#{ENV["HEROKU_RAILS_ENV"] || Rails.env}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   private
