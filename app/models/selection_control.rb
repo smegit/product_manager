b9a56350ad86b6e0edf3a547ec1c2ccb3f36ff27
Jesse Zha <jinji.zha@gmail.com>
2018-09-11 10:17:35 +1000

    add more columns into connected products parts in approval show page, add more columns into conected approvals parts in product show page

--- app/models/selection_control.rb
+++ app/models/selection_control.rb
@@ -4,6 +4,9 @@ class SelectionControl < ApplicationRecord
                   against: [:value],
                   using: { tsearch: { prefix: true, any_word: false } }
   has_paper_trail
+  validates :name, presence: true
+  validates :value, presence: true
+  validates :code, presence: true
 end
 
 
