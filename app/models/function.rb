31162884114b3f5bee7b7dc61ba8a88d4448ad30
Jesse Zha <jinji.zha@gmail.com>
2018-09-11 11:10:08 +1000

    add validation to function and approval edit page

--- app/models/function.rb
+++ app/models/function.rb
@@ -2,6 +2,8 @@ class Function < ApplicationRecord
   include PgSearch
   pg_search_scope :search_functions, against: %i[product_type description],using: { tsearch: { prefix: true, any_word: false } }
   mount_uploader :url, FunctionIconUploader
+  validates :product_type, presence: true
+  validates :function_code, presence: true
 end
 
 
